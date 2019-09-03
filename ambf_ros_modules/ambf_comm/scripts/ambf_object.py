d1c5c8d3b50a2dc084b5eff3e8d4ccdbc0f7abf2
nathaniel <nagoldfarb@wpi.edu>
2019-09-03 12:52:03 -0400

    add postion control mask initlitization

--- ambf_ros_modules/ambf_comm/scripts/ambf_object.py
+++ ambf_ros_modules/ambf_comm/scripts/ambf_object.py
@@ -398,6 +398,7 @@ class Object(WatchDog):
         if len(self._cmd.joint_cmds) != n_jnts:
             self._cmd.joint_cmds = [0.0] * n_jnts 
 
+        self._cmd.position_controller_mask = [0]*n_jnts
         self._cmd.joint_cmds[idx] = effort
         self._cmd.position_controller_mask[idx] = False
 
