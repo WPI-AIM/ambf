9234ae9e333fcf9bb783518a06ae9a855ac3cee0
Vignesh <vigneshvarier@gmail.com>
2020-04-09 19:27:31 -0400

    updated printing, total steps and reward

--- ambf_ros_modules/ambf_comm/scripts/RL/stable_baseline_her.py
+++ ambf_ros_modules/ambf_comm/scripts/RL/stable_baseline_her.py
@@ -33,8 +33,8 @@ def main(env):
     # Reset the model
     env.reset()
     # Train the model
-    model.learn(3000000, log_interval=10,
-                callback=CheckpointCallback(save_freq=50000, save_path="./ddpg_dvrk_tensorboard/"))
+    model.learn(4000000, log_interval=100,
+                callback=CheckpointCallback(save_freq=100000, save_path="./ddpg_dvrk_tensorboard/"))
     model.save("./her_robot_env")
 
 
@@ -47,9 +47,10 @@ def load_model(eval_env):
     for _ in range(1000):
         action, _ = model.predict(obs)
         obs, reward, done, _ = eval_env.step(action)
-        print(obs, reward)
+        print(obs['achieved_goal'][0:3], obs['desired_goal'][0:3], reward)
         if done:
             print("----------------It reached terminal state -------------------")
+            time.sleep(5)
             obs = eval_env.reset()
 
 
