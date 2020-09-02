a605fdc1942e95dd17f5fd227b0e9b29d2450d0f
Nathaniel <nagoldfarb@wpi.edu>
2020-09-01 20:58:22 -0400

    ID working
    ID server working

--- ambf_controller/rbdl_server/src/main.cpp
+++ ambf_controller/rbdl_server/src/main.cpp
@@ -12,11 +12,14 @@
 using namespace RigidBodyDynamics::Math;
 
 
-void print(std::vector <double> const &a) {
+void print(std::vector <double> const &a) 
+{
    std::cout << "The vector elements are : ";
 
    for(int i=0; i < a.size(); i++)
-   std::cout << a.at(i) << ' ';
+   {
+        ROS_INFO( "%f", a.at(i) );
+   }
 }
 
 int main(int argc, char* argv[])
@@ -27,9 +30,11 @@ int main(int argc, char* argv[])
     ROS_INFO("starting");
     ros::ServiceClient client_model = nh.serviceClient<rbdl_server::RBDLModel>("CreateModel");
     ros::ServiceClient client_FD = nh.serviceClient<rbdl_server::RBDLDynamics>("ForwardDynamics");
+    ros::ServiceClient client_ID = nh.serviceClient<rbdl_server::RBDLDynamics>("InverseDynamics");
 
     rbdl_server::RBDLModel model_msg; 
-    rbdl_server::RBDLDynamics dny_msg;  
+    rbdl_server::RBDLDynamics Fordny_msg; 
+    rbdl_server::RBDLDynamics Invdny_msg;  
     
     const int dof = 3;
     std::vector<double> q{{0.0, 0.0, 0.0}};
@@ -39,9 +44,7 @@ int main(int argc, char* argv[])
     
     if(ros::ok)
     {
-
-
-       
+      
         model_msg.request.model = "hello";
         if (client_model.call(model_msg))
         {
@@ -49,27 +52,44 @@ int main(int argc, char* argv[])
         }
         else
         {
-            ROS_ERROR("Failed to call service add_two_ints");
+            ROS_ERROR("Failed to call service model");
             return 1;
         }
 
-                model_msg.request.model = "hello";
-
+                
         
-        dny_msg.request.q = q;
-        dny_msg.request.qd = qd;
-        dny_msg.request.tau = tau;
-        if (client_FD.call(dny_msg))
+        Fordny_msg.request.q = q;
+        Fordny_msg.request.qd = qd;
+        Fordny_msg.request.tau = tau;
+        if (client_FD.call(Fordny_msg))
         {
-            print(dny_msg.response.qdd); 
+            print(Fordny_msg.response.qdd); 
         }
         else
         {
-            ROS_ERROR("Failed to call service add_two_ints");
+            ROS_ERROR("Failed to call service FD");
             return 1;
         }
 
+        Invdny_msg.request.q = q;
+        Invdny_msg.request.qd = qd;
+        Invdny_msg.request.qdd = qdd;
+        if (client_ID.call(Invdny_msg))
+        {
+            print(Invdny_msg.response.tau);
+        }
+        else
+        {
+            ROS_ERROR("Failed to call service ID");
+            return 1;
+        }
+
+
+        
     }
+
+
+
     
 
 }
