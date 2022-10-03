62f3757e5adcf94e7081e795ad20ab63f20cad8c
Adnan Munawar <adnanmunawar@users.noreply.github.com>
2022-10-03 12:13:39 -0400

    Optimized framebuffer rendering/copying for publishing video and depth (#190)

--- ambf_shaders/depth/shader_old.fs
+++ ambf_shaders/depth/shader_old.fs
@@ -1,40 +1,19 @@
 #version 130
 uniform sampler2D diffuseMap;
-uniform vec3 maxWorldDimensions;
-uniform float nearPlane;
-uniform float farPlane;
 uniform mat4 invProjection;
 
 varying vec3 vTexCoord;
 
 void main(void)
 {
-    vec4 texColor = texture2D(diffuseMap, vTexCoord.xy);
+    float z_normalized = texture2D(diffuseMap, vTexCoord.xy).r;
     float x = vTexCoord.x * 2.0 - 1.0;
     float y = vTexCoord.y * 2.0 - 1.0;
-    int b0 = int(texColor.x * 255.0);
-    int b1 = int(texColor.y * 255.0);
-    int b2 = int(texColor.z * 255.0);
-    int b3 = int(texColor.w * 255.0);
-
-    int depth = int(b3 << 8 | b2);
-    float d = float(depth) / float(pow(2.0, 2*8));
-    //
-    // uint depth = uint(b3 << 8 | b2);
-    // float d = float(depth) / float(pow(2.0, 2*8));
-
-    float z = d * 2.0 - 1.0;
+    float z = z_normalized * 2.0 - 1.0;
     vec4 P = vec4(x, y, z, 1.0);
 
     P = invProjection * P;
     P /= P.w;
-
-    float deltaZ = farPlane - nearPlane;
-    float normalized_z = (P.z - nearPlane)/deltaZ;
-
-    // Assuming the frustrum is centered vertically and horizontally
-    float normalized_x = (P.x + maxWorldDimensions.x / 2.0)/maxWorldDimensions.x;
-    float normalized_y = (P.y + maxWorldDimensions.y / 2.0)/maxWorldDimensions.y;
-
-    gl_FragColor = vec4(normalized_x, normalized_y, normalized_z, 1.0);
+    // Change from OGL to AMBF coordinates
+    gl_FragColor = vec4(P.z, P.x, P.y, 1.0);
 }
