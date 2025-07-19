9eb31bbd26525ea539ab8727d3380a22137b20a8
aying2 <aying2@github.com>
2025-07-19 18:28:45 -0400

    made ContactDataLocal a separate ros message

--- ambf_plugins/core/ros_comm_plugin/ObjectCommPlugin.cpp
+++ ambf_plugins/core/ros_comm_plugin/ObjectCommPlugin.cpp
@@ -11,13 +11,15 @@ void fillContactData(afContactEventMap* conEventMap, vector<ambf_msgs::ContactEv
         ambf_msgs::ContactEvent conEventMsg;
         conEventMsg.object_name.data = it.first->getQualifiedIdentifier();
         ambf_msgs::ContactData contDataMsg;
+        ambf_msgs::ContactDataLocal contDataLocalMsg;
         for (int in = 0 ; in < it.second.m_contactData.size() ; in ++){
             contDataMsg.distance.data = it.second.m_contactData[in].m_distance;
             copyVec(&it.second.m_contactData[in].m_P_b_w, &contDataMsg.contact_point);
             copyVec(&it.second.m_contactData[in].m_N_b_w, &contDataMsg.contact_normal);
-            copyVec(&it.second.m_contactData[in].m_P_a_l, &contDataMsg.local_point_a);
-            copyVec(&it.second.m_contactData[in].m_P_b_l, &contDataMsg.local_point_b);
+            copyVec(&it.second.m_contactData[in].m_P_a_l, &contDataLocalMsg.local_point_a);
+            copyVec(&it.second.m_contactData[in].m_P_b_l, &contDataLocalMsg.local_point_b);
             conEventMsg.contact_data.push_back(contDataMsg);
+            conEventMsg.contact_data_local.push_back(contDataLocalMsg);
         }
         conEventMsgVec->push_back(conEventMsg);
     }
