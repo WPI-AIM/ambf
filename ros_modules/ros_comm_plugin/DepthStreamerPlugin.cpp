60abb16d69689a9c39241bfc732b61c1840f97d8
Adnan Munawar <adnan.munawar@live.com>
2025-08-14 14:18:59 -0400

    Cleanup depth point cloud plugin to avoid explicit use or either ROS1 or ROS 2

--- ros_modules/ros_comm_plugin/DepthStreamerPlugin.cpp
+++ ros_modules/ros_comm_plugin/DepthStreamerPlugin.cpp
@@ -25,40 +25,6 @@ int afCameraDepthStreamerPlugin::init(const afBaseObjectPtr a_afObjectPtr, const
 
 void afCameraDepthStreamerPlugin::graphicsUpdate()
 {
-#if AMBF_ROS1
-    if (m_write_count % m_publishInterval == 0){
-        sensor_msgs::PointCloud2Iterator<float> pcMsg_x(*m_depthPointCloudMsg, "x");
-        sensor_msgs::PointCloud2Iterator<float> pcMsg_y(*m_depthPointCloudMsg, "y");
-        sensor_msgs::PointCloud2Iterator<float> pcMsg_z(*m_depthPointCloudMsg, "z");
-        sensor_msgs::PointCloud2Iterator<uint8_t> pcMsg_r(*m_depthPointCloudMsg, "r");
-        sensor_msgs::PointCloud2Iterator<uint8_t> pcMsg_g(*m_depthPointCloudMsg, "g");
-        sensor_msgs::PointCloud2Iterator<uint8_t> pcMsg_b(*m_depthPointCloudMsg, "b");
-
-        int width = m_cameraPtr->m_depthBufferColorImage->getWidth();
-        int height = m_cameraPtr->m_depthBufferColorImage->getHeight();
-
-        double noise = 0;
-
-        for (int idx = 0 ; idx < width * height ; idx++, ++pcMsg_x, ++pcMsg_y, ++pcMsg_z, ++pcMsg_r, ++pcMsg_g, ++pcMsg_b){
-            if (m_cameraPtr->getDepthNoiseModel()->isEnabled()){
-                noise = m_cameraPtr->getDepthNoiseModel()->generate();
-            }
-            *pcMsg_x = m_cameraPtr->getDepthPointCloud()->getData()[idx * m_cameraPtr->getDepthPointCloud()->getNumFields() + 0] + noise;
-            *pcMsg_y = m_cameraPtr->getDepthPointCloud()->getData()[idx * m_cameraPtr->getDepthPointCloud()->getNumFields() + 1];
-            *pcMsg_z = m_cameraPtr->getDepthPointCloud()->getData()[idx * m_cameraPtr->getDepthPointCloud()->getNumFields() + 2];
-
-            *pcMsg_r = m_cameraPtr->m_bufferColorImage->getData()[idx * 4 + 0];
-            *pcMsg_g = m_cameraPtr->m_bufferColorImage->getData()[idx * 4 + 1];
-            *pcMsg_b = m_cameraPtr->m_bufferColorImage->getData()[idx * 4 + 2];
-        }
-
-        m_depthPointCloudMsg->header.frame_id = m_cameraPtr->getName();
-        m_depthPointCloudMsg->header.stamp.fromSec(m_cameraPtr->getRenderTimeStamp());
-
-        m_depthPointCloudPub->publish(m_depthPointCloudMsg);
-    }
-    m_write_count++;
-#elif AMBF_ROS2
     if (m_write_count % m_publishInterval == 0){
         sensor_msgs::PointCloud2Iterator<float> pcMsg_x(*m_depthPointCloudMsg, "x");
         sensor_msgs::PointCloud2Iterator<float> pcMsg_y(*m_depthPointCloudMsg, "y");
@@ -88,10 +54,9 @@ void afCameraDepthStreamerPlugin::graphicsUpdate()
         m_depthPointCloudMsg->header.frame_id = m_cameraPtr->getName();
         m_depthPointCloudMsg->header.stamp = ambf_ral::time_from_seconds(m_cameraPtr->getRenderTimeStamp());
 
-        m_depthPointCloudPub->publish(*m_depthPointCloudMsg);
+        m_depthPointCloudPub->publish(m_depthPointCloudMsg);
     }
     m_write_count++;
-#endif
 }
 
 void afCameraDepthStreamerPlugin::physicsUpdate(double)
