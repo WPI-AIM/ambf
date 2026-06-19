0d88fee33502c93f249a7770f64f35d2004cbd9e
Adnan Munawar <adnan.munawar@live.com>
2026-06-19 00:54:31 -0400

    Remove std::array use as ROS 1 uses boost::array for #260

--- ros_modules/ros_comm_plugin/VideoStreamerPlugin.cpp
+++ ros_modules/ros_comm_plugin/VideoStreamerPlugin.cpp
@@ -1,5 +1,4 @@
 #include "VideoStreamerPlugin.h"
-#include <array>
 #include <cmath>
 
 AF_REGISTER_OBJECT_PLUGIN(afCameraVideoStreamerPlugin);
@@ -50,24 +49,26 @@ void afCameraVideoStreamerPlugin::updateCameraInfoMsg(){
     const double fy = 0.5 * H * proj(1, 1);
     const double cy = 0.5 * H * (1.0 + proj(1, 2));
 
-    std::array<double, 9> K = {fx, s, cx,
-                               0.0, fy, cy,
-                               0.0, 0.0, 1.0};
-    std::array<double, 9> R = {1.0, 0.0, 0.0,
-                               0.0, 1.0, 0.0,
-                               0.0, 0.0, 1.0};
-    std::array<double, 12> P = {fx, s, cx, 0.0,
-                                0.0, fy, cy, 0.0,
-                                0.0, 0.0, 1.0, 0.0};
-
 #if AMBF_ROS1
-    m_cameraInfoMsg.K = K;
-    m_cameraInfoMsg.R = R;
-    m_cameraInfoMsg.P = P;
+    m_cameraInfoMsg.K = {fx, s, cx,
+                         0.0, fy, cy,
+                         0.0, 0.0, 1.0};;
+    m_cameraInfoMsg.R = {1.0, 0.0, 0.0,
+                         0.0, 1.0, 0.0,
+                         0.0, 0.0, 1.0};
+    m_cameraInfoMsg.P = {fx, s, cx, 0.0,
+                         0.0, fy, cy, 0.0,
+                         0.0, 0.0, 1.0, 0.0};;
 #elif AMBF_ROS2
-    m_cameraInfoMsg.k = K;
-    m_cameraInfoMsg.r = R;
-    m_cameraInfoMsg.p = P;
+    m_cameraInfoMsg.k = {fx, s, cx,
+                         0.0, fy, cy,
+                         0.0, 0.0, 1.0};;
+    m_cameraInfoMsg.r = {1.0, 0.0, 0.0,
+                         0.0, 1.0, 0.0,
+                         0.0, 0.0, 1.0};
+    m_cameraInfoMsg.p = {fx, s, cx, 0.0,
+                         0.0, fy, cy, 0.0,
+                         0.0, 0.0, 1.0, 0.0};;
 #endif
 }
 
